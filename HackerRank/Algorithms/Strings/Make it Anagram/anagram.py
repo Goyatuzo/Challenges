844e934dc94e9c67ad23efe218b93de731b71369
Yuto Otaguro <yotagur@gmail.com>
2016-03-22 12:19:53 -0500

    Started SQL sequence.

--- HackerRank/Algorithms/Strings/Make it Anagram/anagram.py
+++ HackerRank/Algorithms/Strings/Make it Anagram/anagram.py
@@ -5,28 +5,35 @@ def chars_to_delete(a, b):
     a.sort()
     b.sort()
 
-    # If one string is longer than the other, the extra chracters will always be deleted.
-    count = abs(len(a) - len(b))
-
-    curr = 'a'
+    count = 0
 
     a_idx = 0
     b_idx = 0
 
+    a_repeat = False
+    b_repeat = False
+
     while a_idx < len(a) and b_idx < len(b):
+        # Determine if there are repeated letters.
+        if a_idx > 0:
+            a_repeat = a[a_idx] == a[a_idx - 1]
+
+        if b_idx > 0:
+            b_repeat = b[b_idx] == b[b_idx - 1]
+
         # If the two characters are equal, increment both indices by one.
         if a[a_idx] == b[b_idx]:
             a_idx += 1
             b_idx += 1
-        # If char of a is one less than char of b, increment a and count by one.
-        elif ord(a[a_idx]) == ord(b[b_idx]) + 1:
+        # Frequencies differ: a has more
+        elif a_repeat and a[a_idx] != b[b_idx]:
             a_idx += 1
             count += 1
         # Inverse of the above scenario.
-        elif ord(b[b_idx]) == ord(a[a_idx]) + 1:
+        elif b_repeat and a[a_idx] != b[b_idx]:
             b_idx += 1
             count += 1
-        # The only other possible case is if b is higher in the alphabet than a.
+        # If different letters
         elif a[a_idx] > b[b_idx]:
             b_idx += 1
             count += 2
@@ -34,4 +41,18 @@ def chars_to_delete(a, b):
             a_idx += 1
             count += 2
 
-    return count
+    # At the end, if the two idx pointers go to the end and there are remainders, add.
+    if a_idx == b_idx:
+        a_rem = len(a) - a_idx
+        b_rem = len(b) - b_idx
+    else:
+        a_rem = 0
+        b_rem = 0
+
+    print a
+    print b
+    print len(a), len(b)
+    print a_idx, b_idx
+    print a_rem, b_rem
+
+    return count + abs(a_rem + b_rem)
--- HackerRank/Algorithms/Strings/Make it Anagram/test_anagram.py
+++ HackerRank/Algorithms/Strings/Make it Anagram/test_anagram.py
@@ -9,10 +9,12 @@ class TestMakeItAnagram(unittest.TestCase):
     def test_same_length(self):
         self.assertEqual(2, chars_to_delete('bd', 'ab'))
         self.assertEqual(1, chars_to_delete('abc', 'abbc'))
+        self.assertEqual(6, chars_to_delete('abc', 'def'))
 
     def test_diff_length(self):
         self.assertEqual(1, chars_to_delete('ab', 'abc'))
         self.assertEqual(3, chars_to_delete('ab', 'acd'))
+        self.assertEqual(5, chars_to_delete('c', 'abcdef'))
 
 
 if __name__ == '__main__':
