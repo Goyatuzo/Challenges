09bf5a1fca943d60c9c9d77edcecda9cb135e8d2
Yuto Otaguro <yotagur@gmail.com>
2016-09-08 22:47:32 -0500

    Kangaroo TypeScript.

---
+++ TypeScript/HackerRank/Algorithm/Implementation/Kangaroo.ts
@@ -0,0 +1,65 @@
+﻿process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array: Array<string>;
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+export interface Kangaroo {
+    x: number;
+    v: number;
+};
+
+export default function willLandOnSameLocation(one: Kangaroo, two: Kangaroo): string {
+
+    if (one.v === two.v) {
+        if (one.x === two.x) {
+            return "YES";
+        }
+        return "NO";
+    }
+
+    // Equation is x1 + v1 * t = x2 + v2 * t
+    // t = (x2 - x1) / (v1 - v2)
+    const t = (two.x - one.x) / (one.v - two.v);
+    
+    if (t > 0) {
+        return "YES";
+    }
+    return "NO";
+}
+
+function main() {
+    var x1_temp = readLine().split(' ');
+    var x1 = parseInt(x1_temp[0]);
+    var v1 = parseInt(x1_temp[1]);
+    var x2 = parseInt(x1_temp[2]);
+    var v2 = parseInt(x1_temp[3]);
+
+    const one: Kangaroo = {
+        x: x1,
+        v: v1
+    };
+
+    const two: Kangaroo = {
+        x: x2,
+        v: v2
+    }
+
+    console.log(willLandOnSameLocation(one, two));
+}
---
+++ TypeScript/__test__/HackerRank/Algorithm/Implementation/kangaroo.test.ts
@@ -0,0 +1,89 @@
+﻿import willLandOnSameLocation, {Kangaroo} from '../../../../HackerRank/Algorithm/Implementation/kangaroo';
+
+describe('Kangaroo', () => {
+    let one: Kangaroo;
+
+    describe('Kangaroo 1: x = 0, v = 5', () => {
+        beforeEach(() => {
+            one = {
+                x: 0,
+                v: 5
+            };
+        });
+
+        it('Kangaroo 2: x = 0, v = 5 should be YES', () => {
+            let two: Kangaroo = {
+                x: 0,
+                v: 5
+            };
+            expect(willLandOnSameLocation(one, two)).toBe("YES");
+        });
+
+        it('Kangaroo 2: x = 1, v = 5 should be NO', () => {
+            let two: Kangaroo = {
+                x: 1,
+                v: 5
+            };
+            expect(willLandOnSameLocation(one, two)).toBe("NO");
+        });
+    });
+
+    describe('Kangaroo 1: x = 5, v = 5', () => {
+        beforeEach(() => {
+            one = {
+                x: 5,
+                v: 5
+            };
+        });
+
+        it('Kangaroo 2: x = 0, v = 1 should be NO', () => {
+            let two: Kangaroo = {
+                x: 0,
+                v: 1
+            };
+            expect(willLandOnSameLocation(one, two)).toBe("NO");
+        });
+
+        it('Kangaroo 2: x = 0, v = 10 should be YES', () => {
+            let two: Kangaroo = {
+                x: 0,
+                v: 10
+            };
+            expect(willLandOnSameLocation(one, two)).toBe("YES");
+        });
+    });
+
+    describe('Kangaroo 1: x = 0, x = 3', () => {
+        beforeEach(() => {
+            one = {
+                x: 0,
+                v: 3
+            };
+        });
+
+        it('Kangaroo 2: x = 4, v = 2', () => {
+            let two: Kangaroo = {
+                x: 4,
+                v: 2
+            };
+            expect(willLandOnSameLocation(one, two)).toBe("YES");
+        });
+    });
+
+    describe('Kangaroo: 1 x = 0, v = 2', () => {
+        beforeEach(() => {
+            one = {
+                x: 0,
+                v: 2
+            };
+        });
+
+        it('Kangaroo 2: x = 5, v = 3', () => {
+            let two: Kangaroo = {
+                x: 5,
+                v: 3
+            };
+            expect(willLandOnSameLocation(one, two)).toBe("NO");
+        });
+    });
+});
