6635652c76e8c21f1bcca10970be0619f04792eb
Yuto Otaguro <yotagur@gmail.com>
2016-09-08 23:22:44 -0500

    CamelCase TypeScript.

---
+++ TypeScript/__test__/HackerRank/Algorithm/Implementation/camelcase.test.ts
@@ -0,0 +1,11 @@
+﻿import camelCaseWordCount from '../../../../HackerRank/Algorithm/Implementation/camelcase';
+
+describe('CamelCase', () => {
+    it('thisIsATest should be 4', () => {
+        expect(camelCaseWordCount('thisIsATest')).toBe(4);
+    });
+
+    it('saveChangesInTheEditor should be 5', () => {
+        expect(camelCaseWordCount('saveChangesInTheEditor')).toBe(5);
+    });
+});
