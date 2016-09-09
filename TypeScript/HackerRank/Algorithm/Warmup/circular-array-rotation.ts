e928c769fa5d83e4f25da1e1a3d12a69d36a6673
Yuto Otaguro <yotagur@gmail.com>
2016-09-08 21:00:49 -0500

    Circular Array Rotation TypeScript.

---
+++ TypeScript/HackerRank/Algorithm/Warmup/circular-array-rotation.ts
@@ -0,0 +1,46 @@
+﻿process.stdin.resume();
+process.stdin.setEncoding("ascii");
+
+let _input = "";
+let inputArray: Array<string>;
+let inputIndex = 0;
+
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+    inputArray = _input.split("\n");
+    main();
+});
+
+export default function rotateArray(input: Array<number>, k: number): Array<number> {
+    while (k-- > 0) {
+        const top = input[input.length - 1];
+
+        input.unshift(top);
+        input.pop();
+    }
+
+    return input;
+}
+
+function readLine() {
+    return inputArray[inputIndex++];
+}
+
+function main() {
+    // Get n, k, q
+    let nkq = readLine().split(' ');
+    const n = parseInt(nkq[0]);
+    const k = parseInt(nkq[1]);
+    let q = parseInt(nkq[2]);
+
+    let numbers = (readLine().split(' ')).map(element => parseInt(element));
+
+    const scrambled = rotateArray(numbers, k);
+
+    while (q-- > 0) {
+        console.log(scrambled[parseInt(readLine())]);
+    }
+}
---
+++ TypeScript/__test__/HackerRank/Algorithm/Warmup/circular-array-rotation.test.ts
@@ -0,0 +1,23 @@
+﻿import rotateArray from '../../../../HackerRank/Algorithm/Warmup/circular-array-rotation';
+
+describe("CircularArrayRotation", () => {
+    describe('Array: [0, 1, 2]', () => {
+        let array: Array<number>;
+
+        beforeEach(() => {
+            array = [0, 1, 2];
+        });
+
+        it('k = 0 should return same array.', () => {
+            expect(rotateArray(array, 0)).toEqual(array);
+        });
+
+        it('k = 1 should return [1, 2, 0].', () => {
+            expect(rotateArray(array, 1)).toEqual([2, 0, 1]);
+        });
+
+        it('k = 2 shoudl return [2, 0, 1].', () => {
+            expect(rotateArray(array, 2)).toEqual([1, 2, 0]);
+        });
+    });
+});
