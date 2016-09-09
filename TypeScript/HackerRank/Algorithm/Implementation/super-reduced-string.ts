2d4e4e7ab7a39377fc06834f03635ed4b9e2eb8d
Yuto Otaguro <yotagur@gmail.com>
2016-09-08 23:45:12 -0500

    Super reduced string TypeScript.

---
+++ TypeScript/HackerRank/Algorithm/Implementation/super-reduced-string.ts
@@ -0,0 +1,25 @@
+﻿export default function removeDuplicate(input: string): string {
+    let prev = input;
+    input = input.replace(/(.)\1{1}/, '');
+
+    while (prev !== input) {
+        prev = input;
+        input = input.replace(/(.)\1{1}/, '');
+    }
+
+    if (input.length == 0) {
+        return "Empty String";
+    }
+    return input;
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+let _input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+    console.log(removeDuplicate(_input));
+});
---
+++ TypeScript/__test__/HackerRank/Algorithm/Implementation/super-reduced-string.test.ts
@@ -0,0 +1,15 @@
+﻿import removeDuplicate from '../../../../HackerRank/Algorithm/Implementation/super-reduced-string';
+
+describe('SuperReducedString', () => {
+    it('aaabccddd should be abd', () => {
+        expect(removeDuplicate('aaabccddd')).toBe('abd');
+    });
+
+    it('baab', () => {
+        expect(removeDuplicate('baab')).toBe('Empty String');
+    });
+
+    it('aa', () => {
+        expect(removeDuplicate('aa')).toBe('Empty String');
+    });
+});
