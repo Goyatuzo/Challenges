111eed25f74857f062245a361e6104ac45b0fd17
Yuto Otaguro <yotagur@gmail.com>
2016-09-09 21:53:16 -0500

    Divisible sum pairs TypeScript.

---
+++ TypeScript/HackerRank/Algorithm/Implementation/divisible-sum-pairs.ts
@@ -0,0 +1,45 @@
+﻿process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array: Array<string>;
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+export default function divisibleSumPairs(a: Array<number>, k: number) {
+    let count = 0;
+
+    for (let i = 0; i < a.length; ++i) {
+        for (let j = i + 1; j < a.length; ++j) {
+            if ((a[i] + a[j]) % k === 0) {
+                ++count;
+            }
+        }
+    }
+
+    return count;
+}
+
+function main() {
+    var n_temp = readLine().split(' ');
+    var n = parseInt(n_temp[0]);
+    var k = parseInt(n_temp[1]);
+    var a = readLine().split(' ');
+    let mapped = a.map(Number);
+
+    console.log(divisibleSumPairs(mapped, k));
+}
---
+++ TypeScript/__test__/HackerRank/Algorithm/Implementation/divisible-sum-pairs.test.ts
@@ -0,0 +1,25 @@
+﻿import divisibleSumPairs from '../../../../HackerRank/Algorithm/Implementation/divisible-sum-pairs';
+
+describe('DivisibleSumPairs', () => {
+    let array: Array<number>;
+
+    describe('[1, 3, 2, 6, 1, 2]', () => {
+        beforeEach(() => {
+            array = [1, 3, 2, 6, 1, 2];
+        });
+
+        it('k = 3', () => {
+            expect(divisibleSumPairs(array, 3)).toBe(5);
+        });
+    });
+
+    describe('[0, 1, 2, 3, 4, 5]', () => {
+        beforeEach(() => {
+            array = [0, 1, 2, 3, 4, 5];
+        });
+
+        it('k = 1', () => {
+            expect(divisibleSumPairs(array, 1)).toBe(15);
+        });
+    });
+});
