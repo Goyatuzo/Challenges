79fafddcfb2d22583da2c254f914815a560b649f
Yuto Otaguro <yotagur@gmail.com>
2017-02-05 13:51:44 -0600

    Sock merchant TS.

---
+++ TypeScript/HackerRank/Algorithm/Implementation/sock-merchant.ts
@@ -0,0 +1,74 @@
+﻿process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array: Array<string> = [];
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+export class PairSocks {
+    private unmatchedSocks: { [color: number]: boolean };
+    private matched: number;
+
+    constructor() {
+        this.unmatchedSocks = {};
+        this.matched = 0;
+    }
+
+    get unmatchedCount(): number {
+        return Object.keys(this.unmatchedSocks).length;
+    }
+
+    get matchedCount(): number {
+        return this.matched;
+    }
+
+    addSock(sockColor: number) {
+        if (sockColor in this.unmatchedSocks) {
+            delete this.unmatchedSocks[sockColor];
+            this.matched++;
+
+            return;
+        }
+
+        this.unmatchedSocks[sockColor] = true;
+    }
+
+    isUnmatched(sockNumber: number): boolean {
+        return sockNumber in this.unmatchedSocks;
+    }
+}
+
+export default function getMatchedSockCount(c: Array<number>): number {
+    let socks = new PairSocks();
+
+    c.forEach(sockNumber => {
+        socks.addSock(sockNumber);
+    });
+
+    return socks.matchedCount;
+}
+
+function main() {
+    var n = parseInt(readLine());
+    var cString = readLine();
+    if (cString) {
+        var cArray = cString.split(' ');
+        var c = cArray.map(Number);
+        console.log(getMatchedSockCount(c));
+    }
+
+}
---
+++ TypeScript/test/HackerRank/Algorithm/Implementation/sock-merchant.ts
@@ -0,0 +1,83 @@
+﻿import { expect } from 'chai';
+
+import getMatchedSockCount, { PairSocks } from '../../../../HackerRank/Algorithm/Implementation/sock-merchant';
+
+describe('SockMerchant', () => {
+    describe('PairSocks class', () => {
+        it('default constructor', () => {
+            let socks = new PairSocks();
+
+            expect(socks.unmatchedCount).to.eql(0);
+        });
+
+        it('add one unmatched sock, and count should be 1.', () => {
+            let socks = new PairSocks();
+            socks.addSock(50);
+
+            expect(socks.unmatchedCount).to.eql(1);
+            expect(socks.isUnmatched(50)).to.eql(true);
+        });
+
+        it('sock match should set count down to 0.', () => {
+            let socks = new PairSocks();
+            socks.addSock(50);
+            socks.addSock(50);
+
+            expect(socks.unmatchedCount).to.eql(0);
+            expect(socks.isUnmatched(50)).to.eql(false);
+        });
+
+        it('add multiple unmatched socks.', () => {
+            let socks = new PairSocks();
+
+            socks.addSock(6);
+            socks.addSock(5);
+            expect(socks.unmatchedCount).to.eql(2);
+
+            socks.addSock(2);
+            expect(socks.unmatchedCount).to.eql(3);
+
+            socks.addSock(3);
+            expect(socks.unmatchedCount).to.eql(4);
+
+            socks.addSock(5);
+            expect(socks.unmatchedCount).to.eql(3);
+        });
+    });
+
+    describe('test actual inputs.', () => {
+        it('Sample Input on Q', () => {
+            expect(getMatchedSockCount([10, 20, 20, 10, 10, 30, 50, 10, 20])).to.eql(3);
+        });
+
+        describe('Test Case #2', () => {
+            it('1/3', () => {
+                expect(getMatchedSockCount([6, 5, 2, 3, 5])).to.eql(1);
+            });
+
+            it('2/3', () => {
+                expect(getMatchedSockCount([2, 2, 1, 1, 5])).to.eql(2);
+            });
+
+            it('3/3', () => {
+                expect(getMatchedSockCount([1, 3, 3, 3, 5])).to.eql(1);
+            });
+
+            it('1/2', () => {
+                expect(getMatchedSockCount([6, 5, 2, 3, 5, 2, 2, 1])).to.eql(2);
+            });
+
+            it('1/2', () => {
+                expect(getMatchedSockCount([6, 5, 2, 3, 5, 2, 2, 1])).to.eql(2);
+            });
+
+            it('2/2', () => {
+                expect(getMatchedSockCount([1, 5, 1, 3, 3, 3, 5])).to.eql(3);
+            });
+
+            it('Complete', () => {
+                expect(getMatchedSockCount([6, 5, 2, 3, 5, 2, 2, 1, 1, 5, 1, 3, 3, 3, 5])).to.eql(6);
+            });
+        });
+    });
+});
