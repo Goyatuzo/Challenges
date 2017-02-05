b6466ec31edc70d43613645a69933e6a7141be5b
Yuto Otaguro <yotagur@gmail.com>
2017-02-05 02:38:39 -0600

    Reconfigure tests to run on mocha.

---
+++ TypeScript/test/HackerRank/Algorithm/Implementation/divisible-sum-pairs.test.ts
@@ -0,0 +1,28 @@
+﻿import { expect } from 'chai';
+
+import divisibleSumPairs from '../../../../HackerRank/Algorithm/Implementation/divisible-sum-pairs';
+
+
+describe('DivisibleSumPairs', () => {
+    let array: Array<number>;
+
+    describe('[1, 3, 2, 6, 1, 2]', () => {
+        beforeEach(() => {
+            array = [1, 3, 2, 6, 1, 2];
+        });
+
+        it('k = 3', () => {
+            expect(divisibleSumPairs(array, 3)).to.eql(5);
+        });
+    });
+
+    describe('[0, 1, 2, 3, 4, 5]', () => {
+        beforeEach(() => {
+            array = [0, 1, 2, 3, 4, 5];
+        });
+
+        it('k = 1', () => {
+            expect(divisibleSumPairs(array, 1)).to.eql(15);
+        });
+    });
+});
