09c87eec07e184a296e15ff7a1f9a7dfaf259ecf
Yuto Otaguro <yotagur@gmail.com>
2017-02-05 12:23:05 -0600

    Complete mars exploration.

---
+++ TypeScript/HackerRank/Algorithm/Strings/mars-exploration.ts
@@ -0,0 +1,45 @@
+﻿process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array: Array<string> = [];
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine(): string {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+export default function findNonSosCharacterCount(str: string): number {
+    let count = 0;
+
+    for (let i = 0; i < str.length; i += 3) {
+        if (str[i] !== "S") {
+            count++;
+        }
+
+        if (str[i + 1] !== "O") {
+            count++;
+        }
+
+        if (str[i + 2] !== "S") {
+            count++;
+        }
+    }
+    
+    return count;
+}
+
+function main() {
+    var S = readLine();
+    console.log(findNonSosCharacterCount(S));
+}
---
+++ TypeScript/test/HackerRank/Algorithm/Strings/mars-exploration.ts
@@ -0,0 +1,17 @@
+﻿import { expect } from 'chai';
+
+import findNonSosCharacterCount from '../../../../HackerRank/Algorithm/Strings/mars-exploration';
+
+describe('MarsExploration', () => {
+    it("SOS should give 0", () => {
+        expect(findNonSosCharacterCount("SOS")).to.eql(0);
+    });
+
+    it("SOSSPSSQSSOR should give 3", () => {
+        expect(findNonSosCharacterCount("SOSSPSSQSSOR")).to.eql(3);
+    });
+
+    it("SOSSOT should give 1", () => {
+        expect(findNonSosCharacterCount("SOSSOT")).to.eql(1);
+    });
+});
