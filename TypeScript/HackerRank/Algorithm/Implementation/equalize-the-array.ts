13ee366ac593e5563643fcf6c350a559cc3dd4f5
Yuto Otaguro <yotagur@gmail.com>
2017-02-07 23:53:11 -0600

    Equalize the array preliminary.

---
+++ TypeScript/HackerRank/Algorithm/Implementation/equalize-the-array.ts
@@ -0,0 +1,57 @@
+﻿process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array: Array<string> = [];
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+export default function equalizeArray(values: Array<number>): number {
+    let count: { [number: number]: number } = {};
+
+    values.forEach(num => {
+        if (num in count) {
+            count[num]++;
+        } else {
+            count[num] = 1;
+        }
+    });
+
+    let min = Number.MAX_SAFE_INTEGER;
+    Object.keys(count).forEach(key => {
+        min = Math.min(count[key], min);
+    });
+
+    let sum = 0;
+    Object.keys(count).forEach(key => {
+        sum += count[key] - min;
+    });
+
+    return sum;
+}
+
+
+function main() {
+    var n = readLine();
+    var str = readLine();
+
+    if (str) {
+        var a_temp = str.split(' ');
+        var ints = a_temp.map(Number);
+
+        console.log(equalizeArray(ints));
+    }
+}
---
+++ TypeScript/test/HackerRank/Algorithm/Implementation/equalize-the-array.ts
@@ -0,0 +1,13 @@
+﻿import { expect } from 'chai';
+
+import equalizeArray from '../../../../HackerRank/Algorithm/Implementation/equalize-the-array';
+
+describe('EqualizeArray', () => {
+    it('Sample Input', () => {
+        expect(equalizeArray([3, 3, 2, 1, 3])).to.eql(2);
+    });
+
+    it('Test Input', () => {
+        expect(equalizeArray([3, 3, 2])).to.eql(1);
+    });
+});
