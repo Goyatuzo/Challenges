928e30747562a753f5ab7d760a2f2ae05054c651
Yuto Otaguro <yotagur@gmail.com>
2017-02-08 21:28:57 -0600

    Complete bon appetit.

---
+++ TypeScript/HackerRank/Algorithm/Implementation/bon-appetit.ts
@@ -0,0 +1,55 @@
+﻿process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array: Array<string> = [];
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+export default function bonAppetit(k: number, b: number, c: Array<number>): string {
+    let paidSum = c.reduce((sum, curr, idx) => {
+        if (idx === k) {
+            return sum;
+        }
+
+        return sum + curr;
+    });
+
+    let expected = paidSum / 2;
+
+    if (b === expected) {
+        return "Bon Appetit";
+    }
+
+    return (b - expected).toString();
+}
+
+
+function main() {
+    let one = readLine();
+    let two = readLine();
+    let three = readLine();
+
+    if (one && two && three) {
+        let oneNums = one.split(' ').map(Number);
+        let c = two.split(' ').map(Number);
+
+        let k = oneNums[1];
+        let b = parseInt(three);
+
+        console.log(bonAppetit(k, b, c));
+    }
+}
---
+++ TypeScript/test/HackerRank/Algorithm/Implementation/bon-appetit.ts
@@ -0,0 +1,17 @@
+﻿import { expect } from 'chai';
+
+import bonAppetit from '../../../../HackerRank/Algorithm/Implementation/bon-appetit';
+
+describe('BonAppetit', () => {
+    describe('Sample Input', () => {
+        it('#1', () => {
+            expect(bonAppetit(1, 12, [3, 10, 2, 9])).to.eql("5");
+        });
+
+        it('#2', () => {
+            expect(bonAppetit(1, 7, [3, 10, 2, 9])).to.eql("Bon Appetit");
+        });
+    });
+
+
+});
