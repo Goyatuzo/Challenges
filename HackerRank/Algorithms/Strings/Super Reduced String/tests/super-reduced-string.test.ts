c99914e0d31c848a467d5714af728f24131ce39f
Yuto Otaguro <yotagur@gmail.com>
2019-10-03 16:42:51 -0500

    Move test files

---
+++ HackerRank/Algorithms/Strings/Super Reduced String/tests/super-reduced-string.test.ts
@@ -0,0 +1,17 @@
+﻿import { expect } from 'chai';
+
+import removeDuplicate from '../super-reduced-string';
+
+describe('SuperReducedString', () => {
+    it('aaabccddd should be abd', () => {
+        expect(removeDuplicate('aaabccddd')).to.eql('abd');
+    });
+
+    it('baab', () => {
+        expect(removeDuplicate('baab')).to.eql('Empty String');
+    });
+
+    it('aa', () => {
+        expect(removeDuplicate('aa')).to.eql('Empty String');
+    });
+});
